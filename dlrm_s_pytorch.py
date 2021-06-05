060991e6b941666b479ab61391929f7c8718516b
Ch4nYH <hfercxx@hotmail.com>
2021-06-05 16:45:28 +0800

    update

--- dlrm_s_pytorch.py
+++ dlrm_s_pytorch.py
@@ -1576,7 +1576,7 @@ def run():
                             optimizer.zero_grad()
                         # backward pass
                         E.backward()
-                        if should_print:
+                        if True:
                             log_iter = nbatches * k + j + 1
                             for name, p in dlrm.named_parameters():
                                 try:
