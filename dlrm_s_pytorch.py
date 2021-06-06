4b76a937a227cd5f5d3e7b6442b978742cbceef5
Ch4nYH <hfercxx@hotmail.com>
2021-06-06 15:31:12 +0800

    update

--- dlrm_s_pytorch.py
+++ dlrm_s_pytorch.py
@@ -1641,7 +1641,7 @@ def run():
                         total_samp = 0
                         from metrics import get_ntk_n
                         ntk = get_ntk_n(train_ld, dlrm_wrap, False, num_batch=5, use_gpu=use_gpu, ndevices=ndevices)
-                        writer.add_histogram_raw("Train/ntk", train_loss, log_iter)
+                        writer.add_histogram_raw("Train/ntk", ntk, log_iter)
 
                     # testing
                     if should_test:
