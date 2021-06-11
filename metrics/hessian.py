b6d0666c0c6fe4f0ec48738cb8945f4f47e5da8d
Ch4nYH <hfercxx@hotmail.com>
2021-06-11 14:39:04 +0800

    update

--- metrics/hessian.py
+++ metrics/hessian.py
@@ -64,13 +64,12 @@ def get_params_grad(model):
     """
     params = []
     grads = []
-    for param in model.parameters():
-        if not param.requires_grad:
+    for name, param in model.named_parameters():
+        if not param.requires_grad or 'emb' in name:
             continue
         params.append(param)
         grads.append(0. if param.grad is None else param.grad)
         
-
     return params, grads
 
 
